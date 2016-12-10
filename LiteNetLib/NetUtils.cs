a736d0c3d5a16a91c535ce47beefa70f2ea3c72b
comicfans44 <comicfans44@gmail.com>
2016-12-10 22:10:19 +0800

    add NetUtils.DetectHost, makes NetUtils public

--- LiteNetLib/NetUtils.cs
+++ LiteNetLib/NetUtils.cs
@@ -26,7 +26,7 @@ namespace LiteNetLib
     }
 #endif
 
-    static class NetUtils
+    public static class NetUtils
     {
         public static int RelativeSequenceNumber(int number, int expected)
         {
@@ -173,6 +173,40 @@ namespace LiteNetLib
         internal static void DebugWriteError(string str, params object[] args)
         {
             DebugWriteLogic(ConsoleColor.Red, str, args);
+        }
+
+        public static IPAddress DetectHost()
+        {
+            foreach (NetworkInterface ni in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (ni.NetworkInterfaceType != NetworkInterfaceType.Wireless80211 && ni.NetworkInterfaceType != NetworkInterfaceType.Ethernet)
+                {
+                    continue;
+                }
+                if (!ni.Name.Contains("以太网") &&
+                        !ni.Name.Contains("本地连接") &&
+                        !ni.Name.ToLower().Contains("local area connection") &&
+                        !ni.Name.ToLower().Contains("wireless network connection"))
+                {
+                    continue;
+                }
+
+                if (ni.OperationalStatus  != OperationalStatus.Up)
+                {
+                    continue;
+                }
+
+                foreach (UnicastIPAddressInformation ip in ni.GetIPProperties().UnicastAddresses)
+                {
+                    if (ip.Address.AddressFamily != System.Net.Sockets.AddressFamily.InterNetwork)
+                    {
+                        continue;
+                    }
+
+                    return ip.Address;
+                }
+            }
+            return null;
         }
     }
 }
